e02b91786be7c156fe571b9c8230f2d87c6b3dd9
Emilien Kenler <hello@emilienkenler.com>
2013-05-28 17:47:10 +0200

    Add API functions
    Signed-off-by: Emilien Kenler <hello@emilienkenler.com>

--- api/index.js
+++ api/index.js
@@ -9,6 +9,18 @@ var client = redis.createClient(6379, '127.0.0.1', {
   detect_buffers: true
 });
 
+function postStart(req, res, next) {
+  res.send(200, {});
+}
+
+function postStop(req, res, next) {
+  res.send(200, {});
+}
+
+function postReset(req, res, next) {
+  res.send(200, {});
+}
+
 function getSeed(req, res, next) {
   client.lrange('seed', 0, -1, function(err, data) {
     if(err) return next(new restify.InternalError(err));
@@ -61,6 +73,15 @@ function postFilter(req, res, next) {
   client.sadd('filter_'+target,keyword);
 }
 
+
+function getDepth(req, res, next) {
+  res.send(200, {});
+}
+
+function postDepth(req, res, next) {
+  res.send(200, {});
+}
+
 var server = restify.createServer({
   name: 'CrawlerAPI'
 });
@@ -68,11 +89,18 @@ server.use(restify.queryParser());
 server.use(restify.bodyParser());
 server.pre(restify.pre.userAgentConnection());
 
+server.post('/start', postStart);
+server.post('/stop', postStop);
+server.post('/reset', postReset);
+
 server.get('/seed', getSeed);
 server.post('/seed', postSeed);
 
 server.get('/filter', getFilter);
 server.post('/filter', postFilter);
+
+server.get('/depth', getDepth);
+server.post('/depth', postDepth);
 
 server.listen(8080, function() {
   console.log('%s listening at %s', server.name, server.url);
