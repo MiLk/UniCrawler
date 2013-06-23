a6dca5ade9caf47cf37bc7d2f05ffea312787041
Emilien Kenler <hello@emilienkenler.com>
2013-06-23 13:54:14 +0200

    [CRAWLER] Reader
    Signed-off-by: Emilien Kenler <hello@emilienkenler.com>

--- crawler/read.js
+++ crawler/read.js
@@ -12,24 +12,29 @@ var client = redis.createClient(6379, '127.0.0.1', {
 
 // When redis is connected
 client.on('ready', function(){
+  fs.unlinkSync('output.csv');
   client.hgetall('encoded_url', function(err, replies) {
     if(err) util.error(err);
     else {
-      _.each(replies, function(encoded_url, url) {
-        fs.appendFileSync('output.csv', url +';');
-          
-        client.lrange('links_'+encoded_url, 0, -1, function(err, replies) {
-          if(err) util.error(err);
+      replies = _.map(replies, function(val, key) { return [key, val]; });
+      async.eachSeries(replies, function(reply, callback) {
+        client.lrange('links_'+reply[1], 0, -1, function(err, replies) {
+          if(err) callback(err);
           else {
-            _.each(replies, function(reply) {
+            async.eachSeries(replies, function(reply, callback2) {
               fs.appendFileSync('output.csv',reply+';');
+              callback2();
+            }, function(err) {
+              if(err) util.log(err);
+              callback();
             });
-            fs.appendFileSync('output.csv',"\n");
           }
+          fs.appendFileSync('output.csv',"\n");
         });
-
+      } , function(err) {
+        if(err) util.log(err);
+        process.exit(0);
       });
-      
     }
   });
 });
