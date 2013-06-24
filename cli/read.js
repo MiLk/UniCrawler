2b8954f52c36246c7700552b2699d555ccaceddd
Emilien Kenler <hello@emilienkenler.com>
2013-06-24 14:12:40 +0200

    [CLI] Add cli reader
    Signed-off-by: Emilien Kenler <hello@emilienkenler.com>

---
+++ cli/read.js
@@ -0,0 +1,13 @@
+var read = require('../api/read')
+  , redis = require('../api/node_modules/redis')
+  ;
+
+var client = redis.createClient(6379, '127.0.0.1', {
+  detect_buffers: true
+});
+
+client.on('ready', function() {
+  read.read(client, function() {
+    process.exit(0);
+  });
+});
